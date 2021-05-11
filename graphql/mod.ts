08fc5fdbf53a2bf603eac8f57e98d1345cdddf4d
Edward Bebbington <edward.bebbington@intercity.technology>
2021-05-11 18:02:35 +0100

    Add graphql as a middleware

---
+++ graphql/mod.ts
@@ -0,0 +1,62 @@
+import { Drash } from "../deps.ts";
+import {
+  ExecutionResult,
+  graphql,
+  GraphQLSchema,
+  renderPlaygroundPage,
+} from "./deps.ts";
+
+interface GraphQLOptions {
+  schema: GraphQLSchema;
+  graphiql: boolean;
+  rootValue: Record<string, () => string>;
+}
+
+/**
+ * Taken from https://github.com/deno-libs/gql/blob/master/http.ts but heavily modified to suit drash's needs,
+ * and to utilise as much of graphql's own code instead
+ */
+export function GraphQL(
+  options: GraphQLOptions,
+): (
+  request: Drash.Http.Request,
+  response: Drash.Http.Response,
+) => Promise<Drash.Http.Response> {
+  return async (request: Drash.Http.Request, response: Drash.Http.Response) => {
+    if (
+      options.graphiql && request.method === "GET" &&
+      (request.headers.get("Accept")?.includes("text/html") ||
+        request.headers.get("Accept")?.includes("*/*"))
+    ) {
+      response.headers.set("Content-Type", "text/html");
+      response.body = renderPlaygroundPage({ endpoint: "/graphql" });
+      return response;
+    }
+
+    if (!["PUT", "POST", "PATCH"].includes(request.method)) {
+      response.status_code = 405;
+      response.body = "Method Not Allowed";
+      return response;
+    }
+
+    const body = request.parsed_body.data as Record<string, unknown>;
+    const query = Object.keys(body)[0]; // Because drahs by default will parse body as application ww form url encoded, so the `body` looks like `{ "{ hello }": undefined }`, which is not the correct format and would need addressing ats ome point as drash should allow the body to be a string too
+    const result = await graphql(
+      options.schema,
+      query,
+      options.rootValue,
+    ) as ExecutionResult;
+    if ("errors" in result) {
+      response.status_code = 400;
+      response.body = "Malformed request body";
+      return response;
+    }
+    response.headers.set("Content-Type", "application/json");
+    response.status_code = 200;
+    response.body = JSON.stringify(result);
+    return response;
+  };
+}
+
+//await graphql(schema, "{ hello }", root)
+//curl -X POST localhost:1337/graphql -d '{ hello }'
---
+++ graphql/tests/mod_test.ts
@@ -0,0 +1,101 @@
+import { Rhum } from "../../test_deps.ts";
+import { GraphQL } from "../mod.ts";
+import { buildSchema } from "./deps.ts";
+import { Drash } from "../../deps.ts";
+
+//
+// DATA
+//
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+const root = {
+  hello: () => {
+    return "Hello world!";
+  },
+};
+const graphQL = GraphQL({ schema, graphiql: true, rootValue: root });
+class GraphQLResource extends Drash.Http.Resource {
+  static paths = ["/graphql"];
+
+  @Drash.Http.Middleware({
+    before_request: [graphQL],
+  })
+  // Used purely as an endpoint for the frontend playground
+  public GET() {
+    return this.response;
+  }
+
+  // Used purely as an endpoint to make a query from the client
+  @Drash.Http.Middleware({
+    before_request: [graphQL],
+    after_request: [],
+  })
+  public POST() {
+    return this.response;
+  }
+}
+const server = new Drash.Http.Server({
+  resources: [GraphQLResource],
+});
+async function serverAction(action: "run" | "close") {
+  if (action === "run") {
+    await server.run({
+      hostname: "localhost",
+      port: 1337,
+    });
+  }
+  if (action === "close") {
+    server.close();
+  }
+}
+
+//
+// TESTS
+//
+Rhum.testPlan("Graphql - mod_test.ts", () => {
+  Rhum.testSuite("GraphQL", () => {
+    Rhum.testCase(
+      "Can respond with a playground when used as middleware",
+      async () => {
+        await serverAction("run");
+        const res = await fetch("http://localhost:1337/graphql");
+        await serverAction("close");
+        const text = await res.text();
+        Rhum.asserts.assertEquals(
+          text.indexOf("<title>GraphQL Playground</title>") > -1,
+          true,
+        );
+        Rhum.asserts.assertEquals(res.status, 200);
+        Rhum.asserts.assertEquals(res.headers.get("Content-Type"), "text/html");
+        Rhum.asserts.assertEquals(true, false); // TODO THIS WHOLE TEST CASE NEEDS OT BE ADDRESSED. the drash server when respondinng with the playground, has a bunch of errors in the console
+      },
+    );
+    Rhum.testCase(
+      "Will make a query on a request when used as middleware",
+      async () => {
+        await serverAction("run");
+        const res = await fetch("http://localhost:1337/graphql", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: "{ hello }",
+        });
+        await serverAction("close");
+        const json = JSON.parse(await res.json());
+        Rhum.asserts.assertEquals(res.status, 200);
+        Rhum.asserts.assertEquals(
+          res.headers.get("Content-Type"),
+          "application/json",
+        );
+        Rhum.asserts.assertEquals(json, { data: { hello: "Hello world!" } });
+      },
+    );
+  });
+});
+
+Rhum.run();
