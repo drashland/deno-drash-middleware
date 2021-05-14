ded9563a2fce824009dfa50ac1ec04f069e71a30
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-05-14 11:31:20 +0100

    organise mod.ts file

--- graphql/mod.ts
+++ graphql/mod.ts
@@ -6,10 +6,68 @@ import {
   renderPlaygroundPage,
 } from "./deps.ts";
 
+type GraphiQLValue = boolean | string;
+
 interface GraphQLOptions {
   schema: GraphQLSchema;
-  graphiql: boolean | string;
+  graphiql: GraphiQLValue;
   rootValue: Record<string, () => string>;
+}
+
+function getPlaygroundEndpoint(graphiql: GraphiQLValue): string | undefined {
+  const playgroundEndpoint = graphiql === true
+    ? "/graphql"
+    : typeof graphiql === "string"
+    ? graphiql
+    : undefined;
+  return playgroundEndpoint;
+}
+
+function requestIsForPlayground(
+  graphiql: GraphiQLValue,
+  request: Drash.Http.Request,
+): false | string {
+  const playgroundEndpoint = getPlaygroundEndpoint(graphiql);
+  if (
+    playgroundEndpoint && request.method === "GET" &&
+    (request.headers.get("Accept")?.includes("text/html") ||
+      request.headers.get("Accept")?.includes("*/*"))
+  ) {
+    return playgroundEndpoint;
+  }
+  return false;
+}
+
+function handleRequestForPlayground(
+  playgroundEndpoint: string,
+  response: Drash.Http.Response,
+): Drash.Http.Response {
+  response.headers.set("Content-Type", "text/html");
+  response.body = renderPlaygroundPage({ endpoint: playgroundEndpoint });
+  return response;
+}
+
+async function executeRequest(
+  options: GraphQLOptions,
+  request: Drash.Http.Request,
+  response: Drash.Http.Response,
+): Promise<Drash.Http.Response> {
+  const body = request.parsed_body.data as Record<string, unknown>;
+  const query = Object.keys(body)[0]; // Because drash by default will parse body as application www form url encoded, so the `body` looks like `{ "{ hello }": undefined }`, which is not the correct format
+  const result = await graphql(
+    options.schema,
+    query,
+    options.rootValue,
+  ) as ExecutionResult;
+  if ("errors" in result) {
+    response.status_code = 400;
+    response.body = "Malformed request body";
+    return response;
+  }
+  response.headers.set("Content-Type", "application/json");
+  response.status_code = 200;
+  response.body = JSON.stringify(result);
+  return response;
 }
 
 /**
@@ -23,19 +81,12 @@ export function GraphQL(
   response: Drash.Http.Response,
 ) => Promise<Drash.Http.Response> {
   return async (request: Drash.Http.Request, response: Drash.Http.Response) => {
-    const playgroundEndpoint = options.graphiql === true
-      ? "/graphql"
-      : typeof options.graphiql === "string"
-      ? options.graphiql
-      : undefined;
-    if (
-      playgroundEndpoint && request.method === "GET" &&
-      (request.headers.get("Accept")?.includes("text/html") ||
-        request.headers.get("Accept")?.includes("*/*"))
-    ) {
-      response.headers.set("Content-Type", "text/html");
-      response.body = renderPlaygroundPage({ endpoint: playgroundEndpoint });
-      return response;
+    const playgroundEndpoint = requestIsForPlayground(
+      options.graphiql,
+      request,
+    );
+    if (playgroundEndpoint) {
+      return handleRequestForPlayground(playgroundEndpoint, response);
     }
 
     if (!["PUT", "POST", "PATCH"].includes(request.method)) {
@@ -44,22 +95,7 @@ export function GraphQL(
       return response;
     }
 
-    const body = request.parsed_body.data as Record<string, unknown>;
-    const query = Object.keys(body)[0]; // Because drash by default will parse body as application www form url encoded, so the `body` looks like `{ "{ hello }": undefined }`, which is not the correct format
-    const result = await graphql(
-      options.schema,
-      query,
-      options.rootValue,
-    ) as ExecutionResult;
-    if ("errors" in result) {
-      response.status_code = 400;
-      response.body = "Malformed request body";
-      return response;
-    }
-    response.headers.set("Content-Type", "application/json");
-    response.status_code = 200;
-    response.body = JSON.stringify(result);
-    return response;
+    return await executeRequest(options, request, response);
   };
 }
 
