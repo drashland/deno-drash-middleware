e301759d8e84169988b60aeb57d31a7396b9a456
drashbot <59404681+drashbot@users.noreply.github.com>
2021-07-21 18:32:33 -0400

    Update dependencies and bump version numbers across repo (#132)
    Co-authored-by: crookse <crookse@users.noreply.github.com>

--- csrf/deps.ts
+++ csrf/deps.ts
@@ -1,2 +1,2 @@
-export { createHash } from "https://deno.land/std@0.101.0/hash/mod.ts";
-export { v4 } from "https://deno.land/std@0.101.0/uuid/mod.ts";
+export { createHash } from "https://deno.land/std@0.102.0/hash/mod.ts";
+export { v4 } from "https://deno.land/std@0.102.0/uuid/mod.ts";
